60602d531f80643baea770db29999a45701a2ea4
JonathanSeife <112643740+JonathanSeife@users.noreply.github.com>
2022-09-06 12:37:24 -0400

    Cleared scripts.js and added an array

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,11 @@
-alert('Hello world');
+let pokemonList = [
+    { name: 'Bulbusuar', height: 0.7, types: ['grass', 'poison']},
+    { name: 'Rattata', height: 0.3, types: ['normal']},
+    { name: 'Pikachu', height: 0.4, types: ['electric']},
+    { name: 'Squirtle', height: 0.5, types: ['water']},
+    { name: 'Jigglypuff', height: 0.5, types: ['fairy', 'normal']}
+];
 
-let favoriteFood = "Chicken Wings";
-document.write(favoriteFood);
+let poke0 = pokemonList[3]
+
+document.write(`${poke0.name} is ${poke0.height} meters tall and it's type is ${poke0.types} .`);
