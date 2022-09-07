5508a7bce6e07f5ed03675eaf517a9190f4f16d2
JonathanSeife <112643740+JonathanSeife@users.noreply.github.com>
2022-09-07 11:32:19 -0400

    added a loop to print pokemonlist with heights

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,15 @@ let pokemonList = [
     { name: 'Jigglypuff', height: 0.5, types: ['fairy', 'normal']}
 ];
 
-let pokeSquirtle = pokemonList[3]
-
-document.write(`${pokeSquirtle.name} is ${pokeSquirtle.height} meters tall and it's type is ${pokeSquirtle.types} .`);
+for ( let i = 0; i < pokemonList.length; i++ ) {
+    if (pokemonList [i].height > 0.6) {
+        document.write(pokemonList[i].name + ' height: ' + pokemonList[i].height + ' Wow, that\'s a big pokemon' + '<br>');
+        // if height of pokemon is equal to or greater than the height 0.6, label pokemon as Wow, that's big
+    }else if (pokemonList[i].height <=0.6 && pokemonList[i].height >=0.4) {
+        document.write(pokemonList[i].name + ' height: ' + pokemonList[i].height + '  that\'s a normal size pokemon' + '<br>');
+        // if height of pokemon is equal to or less than 0.6 and equal to or greater than 0.4, label pokemon as that's a normal size
+    }else {
+        document.write(pokemonList[i].name + ' height: ' + pokemonList[i].height + ' that\'s a small pokemon' + '<br>');
+            // if height of pokemon does not fall into either of the two above categories, label pokemon as small
+    }
+}
