ba0dffce0021eae217cfc4ac00e3d6fc01b1e841
JonathanSeife <112643740+JonathanSeife@users.noreply.github.com>
2022-09-09 13:08:53 -0400

    Wrapped the array in IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,26 @@
-//array of pokemon
-let pokemonList = [
-  { name: "Bulbusuar", height: 0.7, types: ["grass", "poison"] },
-  { name: "Rattata", height: 0.3, types: ["normal"] },
-  { name: "Pikachu", height: 0.4, types: ["electric"] },
-  { name: "Squirtle", height: 0.5, types: ["water"] },
-  { name: "Jigglypuff", height: 0.5, types: ["fairy", "normal"] },
-];
+//wrapping array in IIFE
+let pokemonRepository = (function () {
+  let pokemonList = [
+    { name: "Bulbusuar", height: 0.7, types: ["grass", "poison"] },
+    { name: "Rattata", height: 0.3, types: ["normal"] },
+    { name: "Pikachu", height: 0.4, types: ["electric"] },
+    { name: "Squirtle", height: 0.5, types: ["water"] },
+    { name: "Jigglypuff", height: 0.5, types: ["fairy", "normal"] },
+  ];
+
+  function getAll() {
+    return pokemonList;
+  }
+
+  function add(pokemon) {
+    pokemonList.push(pokemon);
+  }
+
+  return {
+    getAll: getAll,
+    add: add,
+  };
+})();
 
 function myLoopFunction(pokemon) {
   document.write(
@@ -18,4 +33,4 @@ function myLoopFunction(pokemon) {
   );
 }
 
-pokemonList.forEach(myLoopFunction);
+pokemonRepository.getAll().forEach(myLoopFunction);
